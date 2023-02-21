9d86596b099c490cbd071741dc5cabc7855de207
Stephan Bösch-Plepelits <skunk@xover.htu.tuwien.ac.at>
2023-02-21 09:56:27 +0100

    Documentation (JSDoc)

--- drupalRest.js
+++ drupalRest.js
@@ -27,12 +27,22 @@ const entityConfiguration = {
   }
 }
 
-module.exports = class DrupalREST {
+class DrupalREST {
+  /**
+   * @param {Object} options - Options to the API
+   * @param {string} options.url - URL of the Drupal installation
+   * @param {string} [options.user] - Username to login (if empty, use anonymous access)
+   * @param {string} [options.pass] - Password to login
+   */
   constructor (options) {
     this.options = options
     this.sessionHeaders = {}
   }
 
+  /**
+   * Login to website for further requests
+   * @param {function} callback
+   */
   login (callback) {
     if (!this.options.user) {
       // anonymous access
@@ -135,18 +145,46 @@ module.exports = class DrupalREST {
       })
   }
 
+  /**
+   * Load the JSON structure for a node
+   * @param {number} id - ID of the node
+   * @param {Object} [options] - Additional options (currently none defined)
+   * @param {function} callback - The callback will receive (err, content)
+   */
   nodeGet (id, options, callback) {
     this.entityGet('node', id, options, callback)
   }
 
+  /**
+   * Create a node or change an existing node
+   * @param {number|null} id - ID of the node or null to create a new node
+   * @param {Object} content - Content to save
+   * @param {Object} [options] - Additional options (currently none defined)
+   * @param {function} callback - The callback will receive (err, content)
+   */
   nodeSave (id, content, options, callback) {
     this.entitySave('node', id, content, options, callback)
   }
 
+  /**
+   * Remove a node
+   * @param {number} id - ID of the node
+   * @param {Object} [options] - Additional options (currently none defined)
+   * @param {function} callback - The callback will receive (err). If err is null, the node has been successfully removed.
+   */
   nodeRemove (id, options, callback) {
     this.entityRemove('node', id, options, callback)
   }
 
+  /**
+   * Upload a file
+   * @param {Object} file - A structure describing the file
+   * @param {string} file.filename - Filename of the file
+   * @param {*} file.content - Content of the file (whatever fetch accepts, e.g. string, Blob, ...)
+   * @param {string} entityPath - '{entity_type_id}/{type_id}/{field_id}', e.g. 'media/image/field_media_image'.
+   * @param {Object} [options] - Additional options (currently none defined)
+   * @param {function} callback - The callback will receive (err, file)
+   */
   fileUpload (file, entityPath, options, callback) {
     const headers = { ...this.sessionHeaders }
     headers['Content-Type'] = 'application/octet-stream'
@@ -165,50 +203,126 @@ module.exports = class DrupalREST {
       })
 }
 
+  /**
+   * Load the JSON structure for a taxonomy term
+   * @param {number} id - ID of the taxonomy term
+   * @param {Object} [options] - Additional options (currently none defined)
+   * @param {function} callback - The callback will receive (err, content)
+   */
   taxonomyGet (id, options, callback) {
     this.entityGet('taxonomy', id, options, callback)
   }
 
+  /**
+   * Create a node or change an existing taxonomy term
+   * @param {number|null} id - ID of the node or null to create a new node
+   * @param {Object} content - Content to save
+   * @param {Object} [options] - Additional options (currently none defined)
+   * @param {function} callback - The callback will receive (err, content)
+   */
   taxonomySave (id, content, options, callback) {
     this.entitySave('taxonomy', id, content, options, callback)
   }
 
+  /**
+   * Remove a taxonomy term
+   * @param {number} id - ID of the taxonomy term
+   * @param {Object} [options] - Additional options (currently none defined)
+   * @param {function} callback - The callback will receive (err). If err is null, the taxonomy term has been successfully removed.
+   */
   taxonomyRemove (id, options, callback) {
     this.entityRemove('taxonomy', id, options, callback)
   }
 
+  /**
+   * Load the JSON structure for a media entity
+   * @param {number} id - ID of the media entity
+   * @param {Object} [options] - Additional options (currently none defined)
+   * @param {function} callback - The callback will receive (err, content)
+   */
   mediaGet (id, options, callback) {
     this.entityGet('media', id, options, callback)
   }
 
+  /**
+   * Create a media entity or change an existing media entity
+   * @param {number|null} id - ID of the media entity or null to create a new media entity
+   * @param {Object} content - Content to save
+   * @param {Object} [options] - Additional options (currently none defined)
+   * @param {function} callback - The callback will receive (err, content)
+   */
   mediaSave (id, content, options, callback) {
     this.entitySave('media', id, content, options, callback)
   }
 
+  /**
+   * Remove a media entity
+   * @param {number} id - ID of the media entity
+   * @param {Object} [options] - Additional options (currently none defined)
+   * @param {function} callback - The callback will receive (err). If err is null, the media entity has been successfully removed.
+   */
   mediaRemove (id, options, callback) {
     this.entityRemove('media', id, options, callback)
   }
 
+  /**
+   * Load the JSON structure for a user
+   * @param {number} id - ID of the user
+   * @param {Object} [options] - Additional options (currently none defined)
+   * @param {function} callback - The callback will receive (err, content)
+   */
   userGet (id, options, callback) {
     this.entityGet('user', id, options, callback)
   }
 
+  /**
+   * Create a user or change an existing user
+   * @param {number|null} id - ID of the user or null to create a new user
+   * @param {Object} content - The content to save
+   * @param {Object} [options] - Additional options (currently none defined)
+   * @param {function} callback - The callback will receive (err, content)
+   */
   userSave (id, content, options, callback) {
     this.entitySave('user', id, content, options, callback)
   }
 
+  /**
+   * Remove a user
+   * @param {number} id - ID of the user
+   * @param {Object} [options] - Additional options (currently none defined)
+   * @param {function} callback - The callback will receive (err). If err is null, the user has been successfully removed.
+   */
   userRemove (id, options, callback) {
     this.entityRemove('user', id, options, callback)
   }
 
+  /**
+   * Load the JSON structure for a file entity
+   * @param {number} id - ID of the file
+   * @param {Object} [options] - Additional options (currently none defined)
+   * @param {function} callback - The callback will receive (err, content)
+   */
   fileGet (id, options, callback) {
     this.entityGet('file', id, options, callback)
   }
 
+  /**
+   * Change an existing file
+   * @param {number} id - ID of the file
+   * @param {Object} content - The content to change
+   * @param {Object} [options] - Additional options (currently none defined)
+   * @param {function} callback - The callback will receive (err, content)
+   */
   fileSave (id, content, options, callback) {
     this.entitySave('file', id, content, options, callback)
   }
 
+  /**
+   * Remove a file
+   * @param {number} id - ID of the file
+   * @param {Object} [options] - Additional options (currently none defined)
+   * @param {function} callback - The callback will receive (err). If err is null, the file has been successfully removed.
+   */
   fileRemove (id, options, callback) {
     this.entityRemove('file', id, options, callback)
   }
@@ -266,3 +380,5 @@ function processJSONResult (body, callback) {
 
   global.setTimeout(() => callback(error, data), 0)
 }
+
+module.exports = DrupalREST
